3fce416ed0a49d43d0c788f2fd586cd1710c87f6
KrishnaM26 <krishna.mehta26@yahoo.com>
2025-06-13 11:34:04 -0400

    all arrays numpy, reshaped for training'

--- ICT.py
+++ ICT.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import KDTree
 import numpy as np
 import os 
 import cv2
@@ -5,6 +6,11 @@ import cv2
 
 dir_path = '/Users/krishnamehta/Desktop/Image Color Transfer/Kodak Color Plus 200'
 image_path = '/Users/krishnamehta/Desktop/Image Color Transfer/IMG_2559.JPG' 
+
+def get_image_props(path):
+    image_rgb = cv2.imread(path)
+    h, w = image_rgb.shape[:2]
+    return image_rgb, h, w
 
 
 def get_color_palette(path):
@@ -16,30 +22,29 @@ def get_color_palette(path):
         path = os.path.join(dir_path,i)
         image_paths.append(path)
 
-    unique_colors = set()
+    unique_colors = []
     for img in image_paths:
-        image = cv2.imread(img)
-        h, w = image.shape[:2]
+        image, h, w = get_image_props(img)
         full_size = w * h
-        print([w,h], full_size)
+       # print([w,h], full_size)
         for i in range(w):
             for j in range(h):
                 B,G,R = image[j,i]
-                rgb_arr = " ".join([str(R).zfill(3), str(G).zfill(3), str(B).zfill(3)])
-                unique_colors.add(rgb_arr)
+                R, G, B  = str(R).zfill(3), str(G).zfill(3), str(B).zfill(3)
+                #rgb_arr = " ".join([str(R).zfill(3), str(G).zfill(3), str(B).zfill(3)])
+                unique_colors.append([int(R), int(G), int(B)])
             
-        print("Total Unique Colors: ", len(unique_colors))
+        #print("Total Unique Colors: ", len(unique_colors))
         print(f"image {img} complete")
-
-    color_palette = np.array(list(unique_colors))
-    return color_palette
+    
+    color_palette = np.array(unique_colors)
+    unique_palette = np.unique(color_palette, axis=0)
+    return unique_palette
 
 
 def get_image_rgb(path):
 
-    imageTC = cv2.imread(path)
-    h, w = imageTC.shape[:2]
-
+    imageTC, h, w = get_image_props(path) 
     imageTC_rgb = []
     for i in range(w):
         rgb_row = [] 
@@ -49,7 +54,9 @@ def get_image_rgb(path):
             #rgb_str2 = " ".join([str(R).zfill(3), str(G).zfill(3), str(B).zfill(3)])
             rgb_row.append([int(R), int(G), int(B)])
         imageTC_rgb.append(rgb_row)
-    return np.array(imageTC_rgb)
+
+    image_2np =np.array(imageTC_rgb) 
+    return image_2np 
         
 '''
 # Displays Image #
@@ -57,5 +64,15 @@ cv2.imshow("RGB Image", np.array(imageTC_rgb))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
-# print(get_image_rgb(image_path))
-print(get_color_palette(dir_path))
+#colors = get_color_palette(dir_path)
+og_image = get_image_rgb(image_path)
+og_img_shape = og_image.shape
+
+threed_twod = og_image.reshape(-1, og_img_shape[2])
+
+
+#print(colors.shape)
+print( og_image[40] , threed_twod[40])
+
+
+
