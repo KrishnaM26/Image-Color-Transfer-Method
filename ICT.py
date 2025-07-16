a41b7c53ad1a38d1e826c74291b4605b78dd500d
KrishnaM26 <krishna.mehta26@yahoo.com>
2025-07-15 20:04:06 -0400

    slight changes

--- ICT.py
+++ ICT.py
@@ -6,14 +6,14 @@ import os
 import cv2
 
 
-dir_path = '/Users/krishnamehta/Desktop/Image Color Transfer/newDir7'
-#image_path = '/Users/krishnamehta/Desktop/Image Color Transfer/IMG_2559.JPG'
+# Image Path
+dir_path = '/Users/krishnamehta/Desktop/Image Color Transfer/TrainingImgs'
 image_path='/Users/krishnamehta/Desktop/Image Color Transfer/ICT_1.jpg'
-download_dir = r'/Users/krishnamehta/Desktop/Image Color Transfer/img_download'
-k=30
-l=1000
-add_noise = False 
-#j=0.1
+
+# Parameters
+k=10 # KMeans Colors
+l=1000 # Smoothness Of Image
+add_noise = False  # grain
 
 def get_image_props(path):
     image_rgb = cv2.imread(path)
@@ -75,7 +75,7 @@ def get_image_rgb(path, noise):
     return image_2np 
   
 
-def get_lrange(arr, l):
+def get_lrange(arr, lspace):
 
     unique_colors = []
     for i in arr:
@@ -83,7 +83,7 @@ def get_lrange(arr, l):
          rgb = (R / 255.0, G/255.0, B/255.0) 
          H, L, S = colorsys.rgb_to_hls(*rgb)
 
-         l_values = np.linspace(0.0, 1.0, l)
+         l_values = np.linspace(0.0, 1.0, lspace)
 
          for _, l_new in enumerate(l_values):
             r, g, b = colorsys.hls_to_rgb(H, l_new, S)
@@ -98,18 +98,12 @@ def get_lrange(arr, l):
 
 colors = get_color_palette(dir_path)
 og_image = get_image_rgb(image_path, add_noise)
-#print(colors,colors.shape) 
-#print(og_image, og_image.shape)
 
 kmeans = KMeans(n_clusters = k, random_state = 0)
 kmeans.fit(colors)
 
-#labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
 centroids_int = centroids.astype(int)
-
-
-
 
 color_palette_range = get_lrange(centroids_int, l)
 
@@ -135,7 +129,6 @@ new_image_map = np.array(new_image_map)
 output_image = new_image_map.reshape(og_img_shape)
 
 final_image = output_image.astype(np.uint8) 
-#print(final_image[1])
 final_img_rotate = cv2.rotate(final_image, cv2.ROTATE_90_CLOCKWISE)
 flip_image = cv2.flip(final_img_rotate, 1)
 
@@ -145,7 +138,3 @@ cv2.imshow("BGR Image", flip_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#os.chdir(download_dir)
-#file_name = f'KCP_2-9_{k}_{l}_.jpg'
-#cv2.imwrite(file_name, flip_image)
-#print(f'image: {k}, {l}  saved')
